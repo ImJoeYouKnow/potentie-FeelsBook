b901e7ec72cb3f6c7e151783a366b2c0e7efeba8
Joe Potentier <joseph.potentier@gmail.com>
2018-10-01 23:58:34 -0600

    Can detect button clicks

--- app/src/main/java/com/example/potentie_feelsbook/MainActivity.java
+++ app/src/main/java/com/example/potentie_feelsbook/MainActivity.java
@@ -6,9 +6,23 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText noteText;
+    private ListView emotionList;
+    private ArrayList<EmotionEntry> emotions = new ArrayList<EmotionEntry>();
+    private ArrayAdapter<EmotionEntry> adapter;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,7 +53,74 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        emotionList = findViewById(R.id.history_list);
+
+        setupButtons();
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter = new ArrayAdapter<EmotionEntry>(this,
+                R.layout.activity_history, emotions);
+        //emotionList.setAdapter(adapter);
+    }
+
+
+    //https://www.youtube.com/watch?v=GtxVILjLcw8 thanks to "Coding in flow" for the tutorial, this can also be found all over online.
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.angerEmoji:
+                Toast.makeText(this, "angerEmoji was clicked", Toast.LENGTH_SHORT).show();
+                AngerEmotionEntry newEmotion = new AngerEmotionEntry();
+                newEmotion.setDate(new Date());
+                newEmotion.setNote(fetchText());
+                emotions.add(newEmotion);
+                //adapter.notifyDataSetChanged();
+
+
+                //ImageView centerImage = findViewById(R.id.centerImage);
+                //centerImage.setImageResource(R.mipmap.ic_center_anger);
+                //ImageViewAnimatedChange(this, centerImage, R.mipmap.ic_center_anger);
+                break;
+            case R.id.joyEmoji:
+                Toast.makeText(this, "joyEmoji was clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.surpriseEmoji:
+                Toast.makeText(this, "surpriseEmoji was clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.loveEmoji:
+                Toast.makeText(this, "loveEmoji was clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.fearEmoji:
+                Toast.makeText(this, "fearEmoji was clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sadnessEmoji:
+                Toast.makeText(this, "sadnessEmoji was clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void setupButtons(){
+
+        ImageButton angerButton = findViewById(R.id.angerEmoji);
+        ImageButton joyButton = findViewById(R.id.joyEmoji);
+        ImageButton surpriseButton = findViewById(R.id.surpriseEmoji);
+        ImageButton loveButton = findViewById(R.id.loveEmoji);
+        ImageButton fearButton = findViewById(R.id.fearEmoji);
+        ImageButton sadnessButton = findViewById(R.id.sadnessEmoji);
+
+        angerButton.setOnClickListener(this);
+        joyButton.setOnClickListener(this);
+        surpriseButton.setOnClickListener(this);
+        loveButton.setOnClickListener(this);
+        fearButton.setOnClickListener(this);
+        sadnessButton.setOnClickListener(this);
+    }
+
 
     public void openActivityHistory(){
         Intent intent = new Intent(this, HistoryActivity.class);
@@ -54,4 +135,15 @@ public class MainActivity extends AppCompatActivity {
     public void finishActivity(){
         this.finish();
     }
+
+    public String fetchText(){
+        noteText = findViewById(R.id.textInput);
+        String text = noteText.getText().toString();
+        return text;
+    }
+
 }
+
+
+
+
